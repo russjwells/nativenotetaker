b5715e709a4a1366e5855363b7dce3ae67c7e9e0
Russ Wells <russ@russdigital.com>
2016-06-22 20:52:51 -0700

    first commit

---
+++ App/Components/Main.js
@@ -0,0 +1,19 @@
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  StyleSheet
+} = React;
+
+class Main extends React.Component{
+  render(){
+    return (
+      <View>
+        <Text> Testing the Router </Text>
+      </View>
+    )
+  }
+};
+
+module.exports = Main;
---
+++ index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View
+} from 'react-native';
+
+class nativenotetaker extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+      initalRoute={{
+        title: 'Github Notetaker',
+        component: Main
+      }} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('nativenotetaker', () => nativenotetaker);
